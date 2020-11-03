94643b21b43e7219a7236356bf279b3b0805cc56
Cole Leffel <cole_leffel_1987@hotmail.com>
2020-11-03 16:13:34 -0600

    Initial check-in

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyWebCRM.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebCRM.Startup))]
+namespace MyWebCRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
